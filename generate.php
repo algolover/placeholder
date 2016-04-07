ce06f5cf45368197f6848d02229785061bcd480a
algolover <rockin.divyanshu@gmail.com>
2016-04-07 16:26:33 +0530

    Added bootstrap,image text, changed image color

--- generate.php
+++ generate.php
@@ -5,8 +5,33 @@ header('Content-type: image/jpeg');
 $image_width=$_GET['Width'];
 $image_height=$_GET['Height'];
 
+$text="$image_height"."X"."$image_width";
+$text_color="#969696";
+/*
+if($image_height <=100 && $image_width <=100 ){
+	$fontsize=10;
+}else {
+	if($image_height >100 && $image_width<400 ){
+		$fontsize=18;
+	}else{
+		if($image_height >100 && $image_width <1000){
+			$fontsize=30;
+		}
+		else{
+			$fontsize=40;
+		}
+	}
+}*/
+if ($image_width<=$image_height){
+	$fontsize=10+(0.01*$image_width);
+}else{
+	$fontsize=10+(0.05*$image_height);
+
+}
 $image=imagecreate($image_width, $image_height);
-imagecolorallocate($image,255,255,255);
+imagecolorallocate($image,205,204,204);
+imagettftext($image,$fontsize,0,$image_width/2.5,$image_height/2, imagecolorallocate($image,255,254,254)
+, 'bakery.ttf', $text);
 
 
 
--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <html>
 <head>
 <title>Placeholder</title>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <style>
+.normalize{
+	margin:0px;
+}
+  </style>
 </head>
 <body>
-<p>Enter value of height and width of the image you want to generate </p>
+	<div class="container normalize">
+<p><b><h2>Enter value of height and width of the image you want to generate</h2></b> </p>
 <form action="generate.php" method="GET">
 	<li>
 		<label for="Height">Height</label>
@@ -19,10 +26,11 @@
 	</li>
 	<br>
 	<li>
-		<button  type="submit" value="Generate" >Generate</button>
+		<button  type="submit" class="btn btn-large btn-success"value="Generate" >Generate</button>
 	</li>
 		
 	</form>
+</div>
 </body>
 </html>
 
